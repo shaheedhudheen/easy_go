1eb2df25ef5f3901f146c48e4f7515331c398e8f
shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2021-05-30 18:08:38 +0530

    authentication

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:easy_go/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -6,6 +7,8 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  AuthService get auth => AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,21 +19,25 @@ class _SignInState extends State<SignIn> {
             padding: EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
-                SizedBox(height: 70,),
+                SizedBox(
+                  height: 70,
+                ),
                 Image(
                   alignment: Alignment.center,
                   height: 100.0,
                   width: 100.0,
                   image: AssetImage('assets/images/logo.png'),
                 ),
-                SizedBox(height: 40,),
-                Text('Sign In',
-                  textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25,  fontFamily: 'Bold'),
+                SizedBox(
+                  height: 40,
                 ),
-
+                Text(
+                  'Sign In',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontFamily: 'Bold'),
+                ),
                 Padding(
-                  padding:  EdgeInsets.all(20.0),
+                  padding: EdgeInsets.all(20.0),
                   child: Column(
                     children: <Widget>[
                       TextField(
@@ -38,12 +45,9 @@ class _SignInState extends State<SignIn> {
                         decoration: InputDecoration(
                           hintText: 'Email',
                           enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)
-                          ),
+                              borderRadius: BorderRadius.circular(30)),
                           focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)
-                          ),
-                          
+                              borderRadius: BorderRadius.circular(30)),
                           hintStyle: TextStyle(
                             color: Colors.grey,
                             fontSize: 18,
@@ -51,18 +55,17 @@ class _SignInState extends State<SignIn> {
                         ),
                         style: TextStyle(fontSize: 14),
                       ),
-                      SizedBox(height: 10,),
-
+                      SizedBox(
+                        height: 10,
+                      ),
                       TextField(
                         obscureText: true,
                         decoration: InputDecoration(
                           hintText: 'Password',
                           enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)
-                          ),
+                              borderRadius: BorderRadius.circular(30)),
                           focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)
-                          ),
+                              borderRadius: BorderRadius.circular(30)),
                           hintStyle: TextStyle(
                             color: Colors.grey,
                             fontSize: 18,
@@ -70,51 +73,59 @@ class _SignInState extends State<SignIn> {
                         ),
                         style: TextStyle(fontSize: 14),
                       ),
-                      SizedBox(height: 40,),                     
+                      SizedBox(
+                        height: 40,
+                      ),
                     ],
                   ),
                 ),
-                
                 ElevatedButton(
-            onPressed: () {
-             },
-
-             style: ElevatedButton.styleFrom(
-             primary: Color.fromRGBO(108, 99, 255, 1),
-             padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-             shape: new RoundedRectangleBorder(
-             borderRadius: new BorderRadius.circular(50)
-             ),
-             ),            
-            child: Text(
-            'Log In',
-            style: TextStyle(fontFamily: 'Bold', fontSize: 18),
-            ),
-          ),
-
-
-
-                                
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(108, 99, 255, 1),
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(50)),
+                  ),
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                        fontFamily: 'Bold',
+                        fontSize: 18,
+                        color: Colors.black87),
+                  ),
+                ),
                 TextButton(
-                    onPressed: (){
-                      
-                    },
-                    child: Text('Dont have an account, sign up here',
-                    style: TextStyle(fontSize: 15, fontFamily: 'Bold', color: Colors.black54),
-                    ),
-                    ),
-
-                    SizedBox(height: 40,),
-
-                    TextButton(
-                    onPressed: (){
-                      
-                    },
-                    child: Text('Sign in as Anonymous',
-                    style: TextStyle(fontSize: 15, fontFamily: 'Bold', color: Colors.black45),
-                    ),
-                    ),
-
+                  onPressed: () {},
+                  child: Text(
+                    'Dont have an account, sign up here',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Bold',
+                        color: Colors.black87),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                TextButton(
+                  onPressed: () async {
+                    dynamic result = await auth.signInAnon();
+                    if (result == null) {
+                      print('Error Signing In');
+                    } else {
+                      print('signed in');
+                      print(result);
+                    }
+                  },
+                  child: Text(
+                    'Sign in as Anonymous',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Bold',
+                        color: Colors.black45),
+                  ),
+                ),
               ],
             ),
           ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
+
   final FirebaseAuth auth = FirebaseAuth.instance;
+  
+  
 
   //sign in anonymously
 
